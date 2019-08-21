cdf0cf2b873fc325888f03d30ac4326688b97267
AsmaMohammed1 <asmaubii@hotmail.com>
2019-08-22 00:47:07 +0300

    master

---
+++ Day02.py
@@ -0,0 +1,20 @@
+
+# We use Print statement for Display information.
+print("Hi")
+
+print("Hello world") #Try comment in same line.
+
+"""
+I'm Try to write comment
+in more than one line 
+so we use three triple quotes 
+for write multiLine string inside it
+"""
+
+# Try multiLine string inside Print statement.
+print(""" 
+Hello,
+How are you?
+I hope you oky.
+See you soon.
+""")
