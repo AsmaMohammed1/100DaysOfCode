30cf664b12021f545ebb29522d6b4eb22ccc79ef
AsmaMohammed1 <asmaubii@hotmail.com>
2019-08-23 03:51:03 +0300

    Day3

---
+++ Day03.py
@@ -0,0 +1,24 @@
+# Talk about "variables".
+
+# differ value of same variable
+x = 10
+x = "String"
+print(x)
+
+# More than one variable in one Line.
+num1, num2, num3 = "ONE,", "TWO,", "THREE"
+print(num1, num2, num3)
+
+# assign the same value to multiple variables in one line.
+x = y = z = 6
+print("The values of x, y, z :", x, y, z)
+
+# combine both text and a variable.
+name = 'Sara'
+print("Your Name is: "+name)
+
+# use the + character to add a variable to another variable.
+FirstName = "Ali "
+LastName = "Mohammed"
+YourName = FirstName+LastName
+print("Full Name: "+YourName)
