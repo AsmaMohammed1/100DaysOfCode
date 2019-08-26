5f9f26396407de8fdabda00902a6a91c6fad69ed
AsmaMohammed1 <asmaubii@hotmail.com>
2019-08-26 03:41:28 +0300

    Day6

---
+++ Day06.py
@@ -0,0 +1,41 @@
+# Talk about casting
+# int() , float() , str()
+
+print('\n\t-- Players List --\n')
+
+# int()
+name = "Fahad"
+age = int(24)
+Weight = int(67.05)
+height = int("170")
+
+print("Player 1:"
+      "\nName is: "+name+
+      "\nAge is:", age,
+      "\nWeight is:", Weight ,
+      "\nHeight is:", height)
+
+# float()
+name2 = "Nasser"
+age2 = float(22)
+Weight2 = float(72.23)
+height2 = float("169")
+
+print("\nPlayer 2:"
+      "\nName is: "+name2 +
+      "\nAge is:", age2,
+      "\nWeight is:", Weight2,
+      "\nHeight is:", height2)
+
+# str()
+name3 = "Omar"
+age3 = str(23)
+Weight3 = str(70.4)
+height3 = str("175")
+
+print("\nPlayer 3:"
+      "\nName is: "+name3+
+      "\nAge is: "+age3+
+      "\nWeight is: "+Weight3+
+      "\nHeight is: "+height3)
+
