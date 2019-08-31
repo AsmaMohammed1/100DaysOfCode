95d16f966f5096f67f0922c9b2f46d92abcb02e3
AsmaMohammed1 <asmaubii@hotmail.com>
2019-08-31 04:22:33 +0300

    Day10

---
+++ Day10.py
@@ -0,0 +1,46 @@
+# Talk about Operators.
+
+# Operators are used to perform operations on:
+# 1: variables 2: values.
+# Python divides the operators into 7 groups
+
+
+x = 10
+y = 6
+
+# 1 > Arithmetic Operators:
+# used with numeric values to perform common mathematical operations.
+
+print("Addition: {}".format(x+y))
+print("Subtraction: {}".format(x-y))
+print("Multiplication: {}".format(x*y))
+print("Division: {}".format(x/y))
+print("Modulus: {}".format(x%y))
+print("Exponentiation: {}".format(x**y))
+print("Floor Division: {}".format(x//y))
+print('-------------------------------------')
+
+# 2 > Assignment Operators:
+# used to assign values to variables.
+
+# x -=2
+# x *=2
+# x /=2
+# x %=2
+# x //=2
+# x **=2
+# x &=3
+# x |=2
+# x ^=2
+# x >>=2
+# x <<=2
+
+# 3 > Comparison Operators:
+# used to compare two values.
+
+print('x == y ? : {}'.format(x==y))
+print('x != y ? : {}'.format(x!=y))
+print('x Greater than y ? : {}'.format(x>y))
+print('x Less than y ? : {}'.format(x<y))
+print('x Greater than or Equal y ? : {}'.format(x>=y))
+print('x Less than or Equal y ? : {}'.format(x<=y))
