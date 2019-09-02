f9a24c72d6487648e82138e0eb38e3bb690dfd72
AsmaMohammed1 <asmaubii@hotmail.com>
2019-09-02 07:47:26 +0300

    Day12

---
+++ Day12.py
@@ -0,0 +1,18 @@
+# Task 2
+
+txt = " Please, I want {} sandwishes and {} donutes "
+txt = txt.replace('I', 'We')
+sandwishes = 5
+Donutes = 7
+txt = txt.format(sandwishes, Donutes)
+
+
+# Try to change lower letter to upper
+'''
+for i in txt:
+	if i == 'a':
+		j = txt.index(i)
+		k = txt[j].upper()
+		
+print(txt)
+'''
