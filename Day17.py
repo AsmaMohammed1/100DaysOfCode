3d83451f4a70cb6d821151559b7f19a04a9e3aab
AsmaMohammed1 <asmaubii@hotmail.com>
2019-09-06 07:16:35 +0300

    Day17

---
+++ Day17.py
@@ -0,0 +1,23 @@
+# Cont'd Talk about Tuple
+
+ages = (23,19,34,27,22,30,22)
+
+# Check if Item Exists
+print(19 in ages)
+
+# Repeat Item
+print(ages*2)
+
+# To add 2 tuples or more into one tuple
+
+names = ('Bayan','Maha')
+print(names+ages)
+
+# Tuple Length
+print('Length of ages is: ',len(ages))
+
+# use the tuple() constructor to make a tuple
+
+Fruits = tuple(('Orange','Apple','Banana'))
+print(Fruits)
+print(Fruits.index('Orange'))
