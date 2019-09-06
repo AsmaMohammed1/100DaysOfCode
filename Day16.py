98f2e3aec050372fd6e26ebf2b09c05fa5d22fc1
AsmaMohammed1 <asmaubii@hotmail.com>
2019-09-06 07:03:20 +0300

    Day16

---
+++ Day16.py
@@ -0,0 +1,23 @@
+# Talk about Tuple
+# Fixed size
+# use ()
+# can't change size or replace values
+# can't delete any item in tuple just delete all tuple
+# can't add any items
+
+Names = ('Ali','Mohammed','Nasser')
+print(Names)
+colors = ()
+print(colors)
+Nums = (6,)
+print(Nums)
+
+MyTuple = (7,2,4.5,'red',7.8)
+print(MyTuple)
+
+# access tuple items by referring to the index number
+print(Names[2])
+
+# print all items
+for i in Names:
+	print(i)
