16d6d35904a9e0b441ecbaa938407bdcc2d44bcb
AsmaMohammed1 <asmaubii@hotmail.com>
2019-09-14 21:21:55 +0300

    Day25

---
+++ Day25.py
@@ -0,0 +1,16 @@
+# Task4 Q1
+
+
+mySet = {8,7,5,3,1}
+
+print('Before add new Items:',mySet)
+
+mySet.update([4,8,12])
+
+print('After add new Items:',mySet)
+
+mySet.discard(8)
+print('After Remove 8 :',mySet)
+
+mySet.clear()
+print('Empty Set:',mySet)
