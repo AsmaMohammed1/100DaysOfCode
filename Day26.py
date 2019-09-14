5b809a74935e71278898b46cc035c0371bc6c263
AsmaMohammed1 <asmaubii@hotmail.com>
2019-09-14 21:31:22 +0300

    Day26

---
+++ Day26.py
@@ -0,0 +1,12 @@
+# Task4 Q2
+
+MyDict = {
+	'name':'pigeon',
+	'type':'bird',
+	'skin cover':'feathers'
+}
+
+print('Value of type:',MyDict['type'])
+
+MyDict['skin cover'] = 'FEATHERS'
+print('After change value:',MyDict)
