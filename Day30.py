a86bcdedaab0f83e98a621e8c7af2978f6451aaa
AsmaMohammed1 <asmaubii@hotmail.com>
2019-09-19 00:37:48 +0300

    Day30

---
+++ Day30.py
@@ -0,0 +1,29 @@
+# Cont'd Talk about Loops
+
+# range() Function
+
+for i in range(9):
+	print(i)
+
+print('------------------------------')
+for i in range(3,6):
+	print(i)
+
+print('------------------------------')
+
+for i in range(2,6,2):
+	print(i)
+else:
+	print('Finish! ')
+
+x = {1,3,2}
+y = {9,0,8}
+
+for j in x: # Outer loop
+	for b in y: # Inner Loop
+		print(j,b)
+
+
+
+
+
