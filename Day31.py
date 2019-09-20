0f1d5add077e010051ebcd61ad924cfa5b9db11c
AsmaMohammed1 <asmaubii@hotmail.com>
2019-09-20 15:21:05 +0300

    Day31

---
+++ Day31.py
@@ -0,0 +1,24 @@
+# Talk about function
+# there is two type
+# 1 : built-in function , 2 : User-Defined Function
+
+
+# Creating a Function
+
+def cal_age( userInpt ):
+	age = 2019-userInpt
+	return age
+
+
+UserInput = int(input("Enter 'Year' of Your Birthday:\n"))
+age = cal_age(UserInput)
+print('Your age Now: ', age)
+
+def Guess(Name='None'):
+	if Name=='n' or Name=='N':
+		print('Ypur name Noura')
+
+Guess('N')
+print(Guess())
+
+
